b1309ac434199a5604c8260410e5464a4f7275a5
gk2803 <georkyri7@gmail.com>
2025-11-21 19:53:48 +0200

    added tex file-type to denote-file-types

--- denote.el
+++ denote.el
@@ -2331,7 +2331,27 @@ Consult the `denote-file-types' for how this is used."
      :date-value-reverse-function denote-extract-date-from-front-matter
      :link-retrieval-format "[denote:%VALUE%]"
      :link denote-org-link-format
-     :link-in-context-regexp denote-org-link-in-context-regexp))
+     :link-in-context-regexp denote-org-link-in-context-regexp)
+    (tex
+     :extension ".tex"
+     :front-matter my-denote-TeX-front-matter
+     :title-key-regexp "^title\\s-*:"
+     :title-value-function denote-format-string-for-org-front-matter
+     :title-value-reverse-function denote-trim-whitespace
+     :keywords-key-regexp "^tags\\s-*:"
+     :keywords-value-function denote-format-keywords-for-text-front-matter
+     :keywords-value-reverse-function denote-extract-keywords-from-front-matter
+     :signature-key-regexp "^signature\\s-*:"
+     :signature-value-function denote-format-string-for-org-front-matter
+     :signature-value-reverse-function denote-trim-whitespace
+     :identifier-key-regexp "^identifier\\s-*:"
+     :identifier-value-function denote-format-string-for-org-front-matter
+     :identifier-value-reverse-function denote-trim-whitespace
+     :date-key-regexp "^date\\s-*:"
+     :date-value-function denote-date-iso-8601
+     :date-value-reverse-function denote-extract-date-from-front-matter
+     :link denote-org-link-format
+     :link-in-context-regexp denote-org-link-in-context-regexp)
   "Alist of variable `denote-file-type' and their format properties.
 
 Each element is of the form (SYMBOL PROPERTY-LIST).  SYMBOL is one of
